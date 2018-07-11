dcfceb734178aa16e840198dd91e3632ebe46314
Code Chrysalis <codechrysalis@Mac-mini-2.local>
2018-07-11 12:31:00 +0900

    wrote some incorrect code in parseJSON.js

--- src/parseJSON.js
+++ src/parseJSON.js
@@ -1,5 +1,31 @@
-const parseJSON = () => {
-  // YOUR CODE HERE
+const parseJSON = (parseThis) => {
+  let resultStr = "";
+
+  if (typeof parseThis === "number") {
+    resultStr = resultStr + parseThis;
+  }
+
+  if (Array.isArray(parseThis)) {
+    // console.log("IT'S AN ARRAY!");
+    resultStr = `${resultStr}[`;
+    if (parseThis.length > 0) {
+      for (
+        let arrayIterator = 0;
+        arrayIterator < parseThis.length;
+        arrayIterator++
+      ) {
+        parseJSON(parseThis[arrayIterator]);
+      }
+    }
+    resultStr = `${resultStr}]`;
+  }
+  // const recursiveFunction = (recurseThis) => {
+  //   return recurseThis;
+  // };
+  // recursiveFunction(parseThis);
+
+  // console.log("here's the string: ", resultStr);
+  return resultStr;
 };
 
 module.exports = { parseJSON };
