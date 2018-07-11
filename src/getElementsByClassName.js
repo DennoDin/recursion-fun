a313b0e3479c1a38ba4a9e41bd5906e307847e02
Code Chrysalis <codechrysalis@Mac-mini-2.local>
2018-07-11 11:59:44 +0900

    completed getElementsByClassName.js

--- src/getElementsByClassName.js
+++ src/getElementsByClassName.js
@@ -1,5 +1,27 @@
-const getElementsByClassName = () => {
-  // YOUR CODE HERE
+const getElementsByClassName = (targetClassName) => {
+  const docBody = document.body;
+  const result = [];
+  const recursiveFunction = (element) => {
+    const classList = element.classList;
+    if (classList) {
+      const classListIterator = classList.values();
+      for (const value of classListIterator) {
+        if (value === targetClassName) {
+          result.push(element);
+          break;
+        }
+      }
+    }
+    if (typeof element === "object" && element.hasChildNodes() === false) {
+      return;
+    }
+    const children = element.childNodes;
+    for (const things in children) {
+      recursiveFunction(children[things]);
+    }
+  };
+  recursiveFunction(docBody);
+  return result;
 };
 
 module.exports = { getElementsByClassName };
